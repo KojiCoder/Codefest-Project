eec2b43b93198ff52219d073fbc5e7e4729e97ae
Kevin <k0j1c0d3r@gmail.com>
2022-01-12 22:03:28 -0500

    My first commit

---
+++ public/js/script.js
@@ -0,0 +1,75 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+const peers = {}
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+//mute video so that we don't have to hear ourselves
+myVideo.muted = true
+
+//sends video and audio to other people in the room
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+    //receive calls
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+    socket.on('user-connected', (userId, roomId) =>{
+        connectToNewUser(userId, stream)
+        console.log('User connected: ' + userId + ' to ' + roomId)
+    })
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+/*socket.on('user-connected', userId => {
+    console.log('User connected: ' + userId)
+})
+socket.on('user-disconnected', userId => {
+    if (peers[userId]){
+        peers[userId].close()
+    }
+    console.log(userId)
+})*/
+
+socket.on('user-disconnected', (userId, roomId) =>{
+    if(peers[userId]){
+        peers[userId].close()
+        console.log('User disconnected: ' + userId + ' from ' + roomId)
+    }
+})
+
+//makes call when a new user connects to room
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+//load the video onto the site and play it
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+//creates a server
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+//generates id
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+//gets request and response
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+//directs user to the room
+app.get('/:room', (req, res) =>{
+    res.render('room', { roomId: req.params.room })
+})
+
+//when someone connects to the server
+io.on('connection', socket => {
+    //when someone connects to a room
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        //tells people in the room of a user connection into their room
+        socket.broadcast.to(roomId).emit('user-connected', userId, roomId)
+        //when someone disconnects from the server
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomId).emit('user-disconnected', userId, roomId)
+        })
+        //prints out the roomId and the userId
+        //console.log(roomId, userId)
+    })
+})
+
+server.listen(3000)
