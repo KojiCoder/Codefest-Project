884bbfb1003fa02833bd0c723d0a7e3ff9eae686
Kevin <k0j1c0d3r@gmail.com>
2022-01-13 08:16:20 -0500

    website fix v0.4

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const io = require('socket.io')(server)
 //generates id
 const { v4: uuidV4 } = require('uuid')
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 //gets request and response
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
+}).listen(app.get('port'), () => {
+    console.log('App is running, server is listening on port ', app.get('port'));
 })
 
 //directs user to the room
